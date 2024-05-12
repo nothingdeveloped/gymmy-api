129fe6adffb73292a5d3a84d48c74b6dac6bbb25
lalith <kumarlalith5180@gmail.com>
2024-05-12 13:19:25 +0530

    CRUD plans

--- src/api/remote-report/routes/remote-report.js
+++ src/api/remote-report/routes/remote-report.js
@@ -45,5 +45,73 @@ module.exports = {
         // middlewares: ["global::reader"],
       },
     },
+    // Plans
+    //
+    ///
+    //
+
+    {
+      method: "GET",
+      path: "/plan",
+      handler: "remote-report.getAllPlans",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "GET",
+      path: "/plan-images",
+      handler: "remote-report.getTodayPlanImages",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "POST",
+      path: "/plan",
+      handler: "remote-report.postPlan",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "PUT",
+      path: "/plan",
+      handler: "remote-report.updatePlan",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/plan/:id",
+      handler: "remote-report.deletePlan",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/plan/date/:date",
+      handler: "remote-report.deleteDayPlans",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
+    {
+      method: "DELETE",
+      path: "/plan/all",
+      handler: "remote-report.deleteAllPlans",
+      config: {
+        policies: [],
+        // middlewares: ["global::reader"],
+      },
+    },
   ],
 };
