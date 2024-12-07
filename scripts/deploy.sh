4f5d08880aa10227effb85ce6493881d76710e69
kumar <kumarlalith5180@gmail.com>
2024-12-07 23:44:01 +0530

    feat(devops) : Remove ssh_key, hopefully added by circleci

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,23 +12,23 @@ SSH_USER_HOST = "${SSH_USER}@${SSH_HOST}"
 
 # remove previous build
 echo "Removing previous build......."
-ssh -i ${SSH_KEY} ${SSH_USER_HOST} "rm -rf ${SSH_PATH}"
+ssh ${SSH_USER_HOST} "rm -rf ${SSH_PATH}"
 # copy new build
 echo "Deploying new build........"
-scp -i ${SSH_KEY} -r build/* ${SSH_USER_HOST}:${SSH_PATH}/build
-scp -i ${SSH_KEY} -r public/* ${SSH_USER_HOST}:${SSH_PATH}/public
-scp -i ${SSH_KEY} package.json ${SSH_USER_HOST}:${SSH_PATH}
-scp -i ${SSH_KEY} package-lock.json ${SSH_USER_HOST}:${SSH_PATH}
-scp -i ${SSH_KEY} server.js ${SSH_USER_HOST}:${SSH_PATH}
+scp -r build/* ${SSH_USER_HOST}:${SSH_PATH}/build
+scp -r public/* ${SSH_USER_HOST}:${SSH_PATH}/public
+scp package.json ${SSH_USER_HOST}:${SSH_PATH}
+scp package-lock.json ${SSH_USER_HOST}:${SSH_PATH}
+scp server.js ${SSH_USER_HOST}:${SSH_PATH}
 
 # Install node modules
 echo "Installing node modules........"
-ssh -i ${SSH_KEY} ${SSH_USER_HOST} "cd ${SSH_PATH} && npm ci"
+ssh ${SSH_USER_HOST} "cd ${SSH_PATH} && npm ci"
 
 
 # restart
 echo "Restarting server........"
-ssh -i ${SSH_KEY} ${SSH_USER_HOST} "cd ${SSH_PATH} && npm run prod"
+ssh ${SSH_USER_HOST} "cd ${SSH_PATH} && npm run prod"
 
 echo "Done.........."
 
